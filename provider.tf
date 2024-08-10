4f06e01b73b905095c2bd50da34e3a18c4e741f6
pradeep17870yadav <pradeepyadav1788000@gmail.com>
2024-08-10 15:30:07 +0530

    region update

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-tf-state-file-bucket-123"
     key            = "terraform.tfstate"
-    region         = "us-west-2"
+    region         = "eu-west-2"
     encrypt        = true
   }
 }
